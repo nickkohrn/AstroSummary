4dd4ef741439b24ab64734339883260acbc21362
Nick Kohrn <bnkohrn@icloud.com>
2025-03-06 08:34:19 -0500

    Add project files

---
+++ AstroSummary/AstroSummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroSummaryApp.swift
+//  AstroSummary
+//
+//  Created by Nick Kohrn on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AstroSummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AstroSummaryTests/AstroSummaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  AstroSummaryTests.swift
+//  AstroSummaryTests
+//
+//  Created by Nick Kohrn on 3/6/25.
+//
+
+import Testing
+@testable import AstroSummary
+
+struct AstroSummaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
